df0a32a3fe0bf9b2d31402a017ac8e5c1c6557f8
note98333 <touoppo828@gmail.com>
2025-12-04 17:01:20 +0700

    top dashboard date

--- src/app/dashboard/top-dash/top-dash.ts
+++ src/app/dashboard/top-dash/top-dash.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-top-dash',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './top-dash.html',
   styleUrl: './top-dash.css',
 })
 export class TopDash {
-
+  months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  date = new Date();
+  year = this.date.getFullYear();
+  month = this.months[this.date.getMonth()];
+  
 }
